5f5f6c81e9d14114c56779836ee38dde6c36ae05
akash gaikwad <akashpgaikwad10@gmail.com>
2026-09-02 12:03:01 +0530

    Update ConnectSphere

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Sidebar from '../Components/Sidebar'
+import ChatContainer from '../Components/ChatContainer'
+import RightSidebar from '../Components/RightSidebar'
 
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  )
+
+const [selectedUser, setSelectedUser] = useState(false)
+
+return (
+
+<div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+
+<div
+className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl
+h-full min-h-0 overflow-hidden grid grid-cols-1 relative
+
+${
+selectedUser
+? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]'
+: 'md:grid-cols-[1fr_1.5fr]'
+}`}
+
+>
+
+<Sidebar
+selectedUser={selectedUser}
+setSelectedUser={setSelectedUser}
+/>
+
+<ChatContainer
+selectedUser={selectedUser}
+setSelectedUser={setSelectedUser}
+/>
+
+<RightSidebar
+selectedUser={selectedUser}
+/>
+
+</div>
+
+</div>
+
+)
 }
 
 export default HomePage
--- client/src/Pages/ProfilePage.jsx
+++ client/src/Pages/ProfilePage.jsx
@@ -1,11 +1,112 @@
-Simport React from 'react'
+import React, { useState } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+import assets from '../assets/chat-app-assets/assets';
 
 const ProfilePage = () => {
-  return (
-    <div>
-      <h1>profile Page</Page></h1>
-    </div>
-  )
+
+const[selectedImg,setSelectedImg] = useState(null)
+
+const navigate = useNavigate();
+
+const [name,setName] = useState("martin johnson");
+
+const [bio,setBio] = useState("Hi Everyone ,I am using ConnectSphere");
+
+const handlesubmit = async (e)=>{
+
+e.preventDefault();
+
+navigate('/');
+
+}
+
+return (
+
+<div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
+
+<div className="w-5/6 max-w-2xl min-h-[500px] backdrop-blur-2xl text-gray-300 border-2
+border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg
+overflow-hidden">
+
+<form
+onSubmit={handlesubmit}
+className='flex flex-col gap-5 p-10 flex-1'
+>
+
+<h3 className='text-lg '>
+Profile details
+</h3>
+
+<label
+htmlFor="avatar"
+className='flex items-center gap-3 cursor-pointer'
+>
+
+<input
+onChange={(e)=> setSelectedImg(e.target.files[0])}
+type="file"
+name=""
+id="avatar"
+accept='.png,.jpg,.jpeg'
+hidden
+/>
+
+<img
+src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon}
+alt=""
+className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}
+/>
+
+Uploade Profile Image
+
+</label>
+
+<input
+onChange={(e)=>setName(e.target.value)}
+value={name}
+type="text"
+name=""
+id=""
+required
+placeholder='Your Name'
+className='p-2 border
+border-gray-500 rounded-md focus:outline-none focus:ring-2
+focus:ring-violet-500'
+/>
+
+<textarea
+onChange={(e)=>setBio(e.target.value)}
+value={bio}
+placeholder='Write profile bio'
+required
+className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2
+focus:ring-violet-500'
+rows={4}
+></textarea>
+
+<button
+type='submit'
+className='bg-gradient-to-r from-purple-400
+to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'
+>
+Save
+</button>
+
+</form>
+
+<img
+className='max-w-41 aspect-square rounded-full mr-6 max-sm:mt-10'
+src={assets.logo_icon}
+alt=""
+/>
+
+</div>
+
+</div>
+
+)
 }
 
 export default ProfilePage
